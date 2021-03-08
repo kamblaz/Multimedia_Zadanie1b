86c257032d8074b1dd32adff409b74005ac01148
Kamil Błażejczuk <ita-12@sr.dmcs.pl>
2021-03-08 12:54:16 +0100

    commit

--- player.js
+++ player.js
@@ -1,37 +1,96 @@
-document.addEventListener("DOMContentLoaded", function() { start_player(); }, false);
-var player;
+document.addEventListener("DOMContentLoaded", function() { initialiseMediaPlayer(); }, false);
+var mediaPlayer;
 
-function choose_video(name) {
-    player.innerHTML = '';
-    var source = document.createElement('source');
-    source.onended = function(e) {
-        console.log("aaa");
-    };
-    player.appendChild(source);
-    source.setAttribute('src', name);
-
-    player.load();
-    play_vid();
+function initialiseMediaPlayer() {
+    mediaPlayer = document.getElementById('media-video');
+    mediaPlayer.controls = false;
+    mediaPlayer.addEventListener('timeupdate', updateProgressBar, false);
+    mediaPlayer.addEventListener('play', function() {
+        var btn = document.getElementById('play-pause-button');
+        changeButtonType(btn, 'pause');
+    }, false);
+    mediaPlayer.addEventListener('pause', function() {
+        var btn = document.getElementById('play-pause-button');
+        changeButtonType(btn, play);
+    }, false);
+    mediaPlayer.addEventListener('volumechange', function(e) {
+        var btn = document.getElementById('mute-button');
+        if (mediaPlayer.muted) changeButtonType(btn, 'unmute');
+        else changeButtonType(btn, 'mute');
+    }, false);
 }
 
-function start_player() {
-    player = document.getElementById('video_player');
-    player.controls = false;
+function togglePlayPause() {
+    var btn = document.getElementById('play-pause-button');
+    if (mediaPlayer.paused || mediaPlayer.ended) {
+        btn.title = 'pause';
+        btn.innerHTML = 'pause';
+        btn.className = 'pause';
+        mediaPlayer.play();
+    } else {
+        btn.title = 'play';
+        btn.innerHTML = 'play';
+        btn.className = 'play';
+        mediaPlayer.pause();
+    }
 }
 
-function play_vid() {
-    player.play();
+function changeButtonType(btn, value) {
+    btn.title = value;
+    btn.innerHTML = value;
+    btn.className = value;
 }
 
-function pause_vid() {
-    player.pause();
+function stopPlayer() {
+    mediaPlayer.pause();
+    mediaPlayer.currentTime = 0;
 }
 
-function stop_vid() {
-    player.pause();
-    player.currentTime = 0;
+function changeVolume(direction) {
+    if (direction === '+') mediaPlayer.volume += mediaPlayer.volume == 1 ? 0 : 0.1;
+    else mediaPlayer.volume -= (mediaPlayer.volume == 0 ? 0 : 0.1);
+    mediaPlayer.volume = parseFloat(mediaPlayer.volume).toFixed(1);
 }
 
-function change_vol() {
-    player.volume = document.getElementById("change_vol").value;
+function toggleMute() {
+    var btn = document.getElementById('mute-button');
+    if (mediaPlayer.muted) {
+        changeButtonType(btn, 'mute');
+        mediaPlayer.muted = false;
+    } else {
+        changeButtonType(btn, 'unmute');
+        mediaPlayer.muted = true;
+    }
+}
+
+function replayMedia() {
+    resetPlayer();
+    mediaPlayer.play();
+}
+
+function resetPlayer() {
+    progressBar.value = 0;
+    mediaPlayer.currentTime = 0;
+    changeButtonType(playPauseBtn, 'play');
+}
+
+function updateProgressBar() {
+    var progressBar = document.getElementById('progress-bar');
+    var percentage = Math.floor((100 / mediaPlayer.duration) *
+        mediaPlayer.currentTime);
+    progressBar.value = percentage;
+    progressBar.innerHTML = percentage + '% played';
+}
+
+function loadVideo() {
+    for (var i = 0; i < arguments.length; i++) {
+        var file = arguments[i].split('.');
+        var ext = file[file.length - 1];
+        if (canPlayVideo(ext)) {
+            resetMediaPlayer();
+            mediaPlayer.src = arguments[i];
+            mediaPlayer.load();
+            break;
+        }
+    }
 }
